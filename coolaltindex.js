9a1a97f94b4b30b33ed91c666639243d27b88b1b
Neruthes <i@neruthes.xyz>
2022-02-01 20:09:48 +0000

    Regular update '2022-02-01T20:09:48+00:00'

--- coolaltindex.js
+++ coolaltindex.js
@@ -22,7 +22,7 @@ const config = {
     siteName: 'File Server',
     copyrightLine: 'Copyright &copy; Website Owner.'
 };
-configPath = `${fspath}/.coolaltindexconfig.json`;
+configPath = `${process.env.wwwroot}/.coolaltindexconfig.json`;
 if (fs.existsSync(configPath)) {
     let userConfig = JSON.parse(fs.readFileSync(configPath));
     Object.keys(userConfig).map(function (x) {
@@ -31,28 +31,29 @@ if (fs.existsSync(configPath)) {
 };
 
 
+// Generate list of dirs and files
+
 let filesList;
 let dirsList;
 
 const getFilesOrDirsList = function (source, needDir) {
     return fs.readdirSync(source, { withFileTypes: true })
         .filter(node => (needDir ? node.isDirectory() : !node.isDirectory()))
-        .map(node => node.name);
+        .map(node => node.name)
+        .filter(nodeName => nodeName[0] !== '.');
 };
 if (fs.existsSync(fspath)) {
     filesList = getFilesOrDirsList(fspath, false);
     dirsList = getFilesOrDirsList(fspath, true);
-    // filesList = [];
-    // dirsList = [];
 } else {
     console.log(fspath);
     console.log(`404 Not Found. '${fspath}'`);
     process.exit(0);
 };
 
-// console.log(filesList);
-// console.log(dirsList);
-// process.exit('');
+
+// Render output HTML
+
 console.log(ejs.render(fs.readFileSync(__dirname + '/index.ejs').toString(), {
     runtime: {
         env: process.env,
