cc2a44957fae5b6a4ccf3fed3f9ed35417bf591e
Neruthes <i@neruthes.xyz>
2022-02-01 17:34:55 +0000

    Regular update '2022-02-01T17:34:55+00:00'

--- coolaltindex.js
+++ coolaltindex.js
@@ -2,12 +2,68 @@
 
 const fs = require('fs');
 const sh = require('child_process').execSync;
-const ejs = require('ejs')
+const ejs = require('ejs');
 
 
 // Prase Arguments
-const ARG_FSPATH = process.argv[2];
-const ARG_WWWPATH = process.argv[3];
+const wwwpath = decodeURIComponent(process.env.url);
+const fspath = process.env.wwwroot + wwwpath;
+
+let pageChain = null;
+let pageTitle = '(Root)';
+if (wwwpath !== '/') {
+    wwwpathArr = wwwpath.split('/');
+    pageChain = wwwpathArr.slice(1, -2);
+    pageTitle = wwwpathArr.slice(-2, -1);
+};
+
+// Initialize configuration
+const config = {
+    siteName: 'File Server',
+    copyrightLine: 'Copyright &copy; Website Owner.'
+};
+configPath = `${fspath}/.coolaltindexconfig.json`;
+if (fs.existsSync(configPath)) {
+    let userConfig = JSON.parse(fs.readFileSync(configPath));
+    Object.keys(userConfig).map(function (x) {
+        config[x] = userConfig[x];
+    });
+};
 
 
+let filesList;
+let dirsList;
+
+if (fs.existsSync(fspath)) {
+    const getFilesOrDirsList = function (source, needDir) {
+        return fs.readdirSync(source, { withFileTypes: true })
+        .filter(node => (needDir ? node.isDirectory() : !node.isDirectory()))
+        .map(node => node.name)
+    };
+    filesList = getFilesOrDirsList(fspath, false);
+    dirsList = getFilesOrDirsList(fspath, true);
+    // filesList = [];
+    // dirsList = [];
+} else {
+    console.log(`404 Not Found`);
+    process.exit(1);
+};
+
+// console.log(filesList);
+// console.log(dirsList);
+// process.exit('');
+console.log(ejs.render(fs.readFileSync(__dirname + '/index.ejs').toString(), {
+    runtime: {
+        env: process.env,
+        fs: fs,
+        sh: sh
+    },
+    pageChain: pageChain,
+    pageTitle: pageTitle,
+    config: config,
+    wwwpath: wwwpath,
+    fspath: fspath,
+    filesList: filesList,
+    dirsList: dirsList
+}))
 
